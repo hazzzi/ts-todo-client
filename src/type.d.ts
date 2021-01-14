cb7aea1e72965626bd6edb575586e462a843d624
hazzzi <hanee_l@naver.com>
2021-01-14 20:34:48 +0900

    todo 타입 추가

---
+++ src/type.d.ts
@@ -0,0 +1,21 @@
+// .d.ts 전역으로 사용할 타입
+
+interface ITodo {
+    _id: string
+    name: string
+    description: string
+    status: string
+    createdAt?: string
+    updatedAt?: string
+}
+
+interface TodoProps {
+    todo: ITodo
+}
+
+type ApiDataType = {
+    message: string
+    status: string
+    todos: ITodo[]
+    todo?: ITodo
+}
